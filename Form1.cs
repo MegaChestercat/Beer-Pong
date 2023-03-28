358bb6475e561b94a2c490ad33011308742675c2
hectorBoi <hector.emiliano.zg@gmail.com>
2023-03-27 18:32:21 -0600

    ignored elements

--- Form1.cs
+++ Form1.cs
@@ -11,10 +11,13 @@ namespace BeerPong
         VSolver solver;
         List<Glass> glasses;
         Pen backTrajectory;
+        Projectile theBall;
         Point mouse, trigger;
         bool isMouseDown, isLeftButton;
         int ballID;
         int countdown = 120;
+        Vec2 startPos;
+        
 
 
         private readonly KonamiSequence _konamiSequence = new KonamiSequence();
@@ -27,6 +30,9 @@ namespace BeerPong
 
         private void Init()
         {
+            startPos = new Vec2(100, 100);
+            theBall= new Projectile(startPos);
+
             canvas = new Canvas(PCT_CANVAS.Size);
             PCT_CANVAS.Image = canvas.bitmap;
             boxes = new List<VBox>();//new VBox(100, 150, 50, 50, 1);
@@ -38,6 +44,46 @@ namespace BeerPong
             canvas.FastClear();
 
             level1();
+
+            //solver = new VSolver();
+            /*
+            a = new VPoint(rnd.Next(150, 500), rnd.Next(150, 400));
+            b = new VPoint(rnd.Next(150, 500), rnd.Next(150, 400));
+            c = new VPoint((int)a.Pos.X, (int)a.Pos.Y);
+            d = new VPoint((int)c.Pos.X + 80, (int)a.Pos.Y);
+            pole = new VPole(a, b);
+            pole2 = new VPole(c, d);*/
+            //box.DrawBody(ref canvas);
+            boxes.Add(new VBox(200, 150, 50, 50, balls.Count));
+            balls.Add(boxes[boxes.Count - 1].a);
+            balls.Add(boxes[boxes.Count - 1].b);
+            balls.Add(boxes[boxes.Count - 1].c);
+            balls.Add(boxes[boxes.Count - 1].d);
+
+
+            /*
+            glasses.Add(new Glass());
+            balls.Add(glasses[glasses.Count - 1].a);
+            balls.Add(glasses[glasses.Count - 1].b);
+            balls.Add(glasses[glasses.Count - 1].c);
+            balls.Add(glasses[glasses.Count - 1].d);
+            balls.Add(glasses[glasses.Count - 1].e);
+            balls.Add(glasses[glasses.Count - 1].f);
+            balls.Add(glasses[glasses.Count - 1].g);
+            balls.Add(glasses[glasses.Count - 1].h);
+            balls.Add(glasses[glasses.Count - 1].i);
+            balls.Add(glasses[glasses.Count - 1].j);
+            balls.Add(glasses[glasses.Count - 1].k);
+            balls.Add(glasses[glasses.Count - 1].l);
+            */
+            //glass.DrawBody(ref canvas);
+            //platform.DrawBody(ref canvas);
+            /*
+            pole.Render(canvas.g, PCT_CANVAS.Width, PCT_CANVAS.Height);
+            a.Render(canvas.g, PCT_CANVAS.Width, PCT_CANVAS.Height);
+            b.Render(canvas.g, PCT_CANVAS.Width, PCT_CANVAS.Height);
+            c.Render(canvas.g, PCT_CANVAS.Width, PCT_CANVAS.Height);
+            d.Render(canvas.g, PCT_CANVAS.Width, PCT_CANVAS.Height);*/
             PCT_CANVAS.Invalidate();
         }
 
@@ -57,6 +103,7 @@ namespace BeerPong
            
             PCT_CANVAS.Invalidate();
         }
+
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
@@ -123,6 +170,65 @@ namespace BeerPong
             balls.Add(boxes[boxes.Count - 1].d);
         }
 
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+                        canvas.FastClear();
+            
+            ballID = solver.CheckCollisions(canvas.g, (int)canvas.Width, (int)canvas.Height, mouse, isMouseDown);
+            theBall.lilbol.Render(canvas.g, PCT_CANVAS.Width, PCT_CANVAS.Height);
+            for (int counter = 0; counter < boxes.Count; counter++)
+            {
+                boxes[counter].React(ref canvas, balls, PCT_CANVAS.Width, PCT_CANVAS.Height);
+            }
+            //box.DrawBody(ref canvas);
+            //glass.DrawBody(ref canvas);
+            //platform.DrawBody(ref canvas);
+
+            if (isMouseDown && isLeftButton && ballID != -1)
+                canvas.g.DrawLine(backTrajectory, balls[ballID].X, balls[ballID].Y, trigger.X, trigger.Y);
+            /*
+            pole.Render(canvas.g, PCT_CANVAS.Width, PCT_CANVAS.Height);
+            a.Render(canvas.g, PCT_CANVAS.Width, PCT_CANVAS.Height);
+            b.Render(canvas.g, PCT_CANVAS.Width, PCT_CANVAS.Height);
+            c.Render(canvas.g, PCT_CANVAS.Width, PCT_CANVAS.Height);
+            d.Render(canvas.g, PCT_CANVAS.Width, PCT_CANVAS.Height);*/
+            PCT_CANVAS.Invalidate();
+        }
+
+        private void PCT_CANVAS_MouseHover(object sender, EventArgs e)
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+
+            int xCoordinate = Cursor.Position.X;
+            int yCoordinate = Cursor.Position.Y;
+
+            if (isInside((int)theBall.lilbol.Pos.X, (int)theBall.lilbol.Pos.Y, (int)theBall.lilbol.radius, xCoordinate, yCoordinate))
+            {
+                Cursor.Current = Cursors.Hand;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isInside((int)theBall.lilbol.Pos.X, (int)theBall.lilbol.Pos.Y, (int)theBall.lilbol.radius, e.X - PCT_CANVAS.Location.X, e.Y - PCT_CANVAS.Location.Y))
+            {
+                Cursor.Current = Cursors.Hand;
+                theBall.selected = true;
+            }
+            else
+            {
+                theBall.selected = false;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (theBall.selected)
+            {
+
+            }
+        }
+
         private void gameTimer_Tick(object sender, EventArgs e)
         {
             if (countdown <= 0)
@@ -136,6 +242,20 @@ namespace BeerPong
             {
                 countdown--;
                 timerCounter.Text = countdown.ToString();
+            }
+        }
+
+        static bool isInside(int circle_x, int circle_y,
+                                  int rad, int x, int y)
+        {
+            // Compare radius of circle with
+            // distance of its center from
+            // given point
+            if ((x - circle_x) * (x - circle_x) +
+                (y - circle_y) * (y - circle_y) <= rad * rad)
+                return true;
+            else
+                return false;
             }
         }
 
